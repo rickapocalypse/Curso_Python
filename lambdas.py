3d147ce3be316aa06db0077bc5e69d1104ed5037
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-04-29 00:19:56 -0400

    utilizando funcao com lambdas

---
+++ lambdas.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def func(a,b,c):
+    return lambda x: a*x**2 + b*x + c
+
+funcao = func(-.1,20,3)
+func_plt = []
+for n in range(200):
+   func_plt.append(funcao(n))
+plt.plot(func_plt)
+plt.show()
