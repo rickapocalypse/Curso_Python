e5a768b8560e95e448a8856b4d740f87446470b7
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-05-25 01:21:22 -0400

    att lanchonete 25/05/2022

--- lanchonete_baguncinha.py
+++ lanchonete_baguncinha.py
@@ -1,44 +1,179 @@
+import math
+import os
+
+productsFood = [
+    {
+        "id": 1, 
+        "name": 'baguncinha',
+        "price": 10
+    },
+    {
+        "id": 2, 
+        "name": 'x-tudo',
+        "price": 0.10
+    },
+    {
+        "id": 3, 
+        "name": 'x-podrao',
+        "price": 0.10
+    }
+]
+
+productsFoodLabels = []
+for pF in productsFood:
+    productsFoodLabels.append(
+        str(pF["id"]) + 
+        " - " + 
+        pF["name"] + 
+        "......." + 
+        "R$ " + 
+        str(pF["price"])
+    )
+
+menus = {
+    "start": {
+        "title": "Menu inicial",
+        "options": [
+            '1 - lanches',
+            '2 - bebidas',
+            '3 - mostrar pedido',
+            '4 - finalizar'
+        ]
+    },
+    "food": {
+        "title": "Menu de lanches",
+        "options": [
+            *productsFoodLabels,
+            '4 - voltar ao menu inicial'
+        ]
+    },
+    "drink" :{
+        "title": "Menu de bebidas",
+        "options" :[
+            '1 - suco',
+            '2 - refrigerante',
+            '3 - cerveja',
+            '4 - voltar ao menu inicial'
+        ]
+    },
+    "order" :{
+        "title": "Revisão de pedido",
+        "options" :[
+            '1 - Alterar pedido',
+            '2 - voltar ao menu inicial',
+            '3 - finalizar pedido'
+        ]
+    }
+}
+
+def isNaN(value):
+    try:
+        math.isnan(float(value))
+        return True
+    except:
+        return False
+
+def inputNumber(label):
+    option = input(label)
+    if(isNaN(option)):
+        option = int(option)
+        return option
+    else:
+        print("Digite uma opção valida...")
+        return False
+
+def printMenu(menuName):
+    menu = menus[menuName]
+    print(menu["title"])
+    print('Escolha uma opção:')
+    for option in menu["options"]:
+        print(option)
+
 def limpTela():
-    print('\n' * 100)
+    os.system('cls')
 
-def menu1():
-    print('Escolha uma opção')
-    print('1 - lanches')
-    print('2 - bebidas')
-    print('3 - mostrar pedido')
-    print('4 - finalizar')
-    x = int(input(f'No que posso te ajudar? '))
+# Inicial
+def menuInicialService(option):
+    match option:
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case 4:
+            return 4
+
+def menuInicial():
+    printMenu('start')
+    option = inputNumber(f'No que posso te ajudar? ')
+    if(option):
+        return menuInicialService(option)
+    else:
+        print("Digite uma opção valida...")
+        menuInicial()
     limpTela()
-    return x
+# ==============
 
-def menu2():
-    print('1 - baguncinha')
-    print('2 - x-tudo')
-    print('3 - x-podrao')
-    print('4 - voltar ao menu inicial')
-    x = int(input(f'No que posso te ajudar? '))
+# Lanches
+def menuLanchesService(option):
+    match option:
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case 4:
+            return 4
+
+def menuLanches():
+    printMenu('food')
+    option = int(input(f'No que posso te ajudar? '))
     limpTela()
-    return x
+    return menuLanchesService(option)
+# ==============
 
-def menu3():
-    print('1 - suco')
-    print('2 - refrigerante')
-    print('3 - cerveja')
-    print('4 - voltar ao menu inicial')
-    x = int(input(f'No que posso te ajudar? '))
+# Bebida
+def menuBebidaService(option):
+    match option:
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case 4:
+            return 4
+
+def menuBebida():
+    printMenu('drink')
+    menuBebidaOption = int(input(f'No que posso te ajudar? '))
     limpTela()
-    return x
+    return menuBebidaService(menuBebidaOption)
+# ==============
 
-def menu4():
-    print(carrinho1())
-    print('1 - Alterar pedido')
-    print('2 - voltar ao menu inicial')
-    print('3 - finalizar pedido')
-    x = int(input(f'No que posso te ajudar? '))
+# Pedido
+def menuPedidoService(option):
+    match option:
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case 4:
+            return 4
+
+def menuPedido():
+    print(carrinho())
+    printMenu('order')
+    menuPedidoOption = int(input(f'No que posso te ajudar? '))
     limpTela()
-    return x
+    return menuPedidoService(menuPedidoOption)
+# ==============
 
-def carrinho1():
+def carrinho():
     carrinho = {}
     for n in pedido:
         c = pedido.count(n)
@@ -48,57 +183,75 @@ def carrinho1():
 
 pedido = []
 
-while True:
-    x = menu1()
-    if x == 1:
-        x = menu2()
-        if x == 1:
-            print('baguncinha adicionada ao pedido')
-            pedido.append('baguncinha')
-        elif x == 2:            
-            print('x-tudo foi adicionado ao pedido')
-            pedido.append('x-tudo')
-        elif x == 3:
-            print('x-podrao foi adicionado ao pedido')
-            pedido.append('x-podrao')
-        elif x == 4:
-            print('voltar ao menu inicial')
-    elif x == 2:
-        x = menu3()
-        if x == 1:
-            print('suco foi adicionado ao pedido')
-            pedido.append('suco')
-        elif x == 2:
-            print('refrigerante foi adicionado ao pedido')
-            pedido.append('refrigerante')
-        elif x == 3:
-            print('cerveja foi adicionado ao pedido')
-            pedido.append('cerveja')
-        elif x == 4:
-            print('voltar ao menu inicial')
-    elif x == 3:
-        x = menu4()
-        if x == 1:
-            c = 1
-            print('0 - Para voltar ao menu inicial')
+def start():
+    while True:
+        menuInicialOption = menuInicial()
+        limpTela()
+        match menuInicialOption:
+            # Lanches
+            case 1:
+                menuLanchesOption = menuLanches()
+                print(productsFood[menuLanchesOption - 1]["name"])
+                pedido.append(productsFood[menuLanchesOption - 1]["name"])
+                # match menuLanchesOption:
+                #     case 1:
+                #         print('baguncinha adicionada ao pedido')
+                #         pedido.append('baguncinha')
+                #     case 2:            
+                #         print('x-tudo foi adicionado ao pedido')
+                #         pedido.append('x-tudo')
+                #     case 3:
+                #         print('x-podrao foi adicionado ao pedido')
+                #         pedido.append('x-podrao')
+                #     case 4:
+                #         print('voltar ao menu inicial')
             
-            for n in pedido:
-                print(f'{c} - {n}')
-                c += 1
-            p = int(input(('Deseja retirar ou voltar ao menu: ')))
-            
-            if p == 0:
-                print('voltar ao menu inicial')
-            else:
-                pedido.pop(p-1)
-                print(carrinho1())
-        
-        elif x == 2:
-            print('voltar ao menu inicial')
-        elif x == 3:
-            print(f'Seu pedido é: {pedido}')
-            break
-    elif x == 4:
-        print('Pedido finalizado')
-        print(f'O seu pedido é: {pedido}')
-        break
+            # Bebidas
+            case 2:
+                menuBebidaOption = menuBebida()
+                match menuBebidaOption:
+                    case 1:
+                        print('suco foi adicionado ao pedido')
+                        pedido.append('suco')
+                    case 2:
+                        print('refrigerante foi adicionado ao pedido')
+                        pedido.append('refrigerante')
+                    case 3:
+                        print('cerveja foi adicionado ao pedido')
+                        pedido.append('cerveja')
+                    case 4:
+                        print('voltar ao menu inicial')
+            # Pedido
+            case 3:
+                menuPedidoOption = menuPedido()
+                match menuPedidoOption:
+                    case 1:
+                        c = 1
+                        print('0 - Para voltar ao menu inicial')
+                        
+                        for n in pedido:
+                            print(f'{c} - {n}')
+                            c += 1
+                        p = int(input(('Deseja retirar ou voltar ao menu: ')))
+                        
+                        if p == 0:
+                            print('voltar ao menu inicial')
+                        else:
+                            pedido.pop(p-1)
+                            print(carrinho())
+                    
+                    case 2:
+                        print('voltar ao menu inicial')
+                    case 3:
+                        print(f'Seu pedido é: {pedido}')
+                        break
+
+            # Finalizar 
+            case 4:
+                print('Pedido finalizado')
+                print(f'O seu pedido é: {carrinho()}')
+                break
+
+            case _:
+                break
+start()
---
+++ modulo_random.py
@@ -0,0 +1,8 @@
+'''
+Módulo Random e o que são módulo?
+
+- Em python, módulos são arquivos que contém funções, variáveis, classes, etc.
+
+Módulo random -> Módulo que contém funções que geram números pseudo-aleatórios.
+'''
+
